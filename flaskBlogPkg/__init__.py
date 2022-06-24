015fe2cb0f89b49665490242039be60e99918efb
Abe Mathew Mankavil <abe.m.mankavil@gmail.com>
2022-06-24 02:43:16 -0400

    Used python-decouple to hide SECRET_KEY

--- flaskBlogPkg/__init__.py
+++ flaskBlogPkg/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from decouple import config
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 #template_folder **kwargs is used to change default "template" folder name, in this case to HTML templates
 app = Flask(__name__, template_folder="HTML templates")
 
-app.config['SECRET_KEY'] = '34665436c8c5f81e7aadc5b9b015089c'
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_man = LoginManager(app)
 
 from flaskBlogPkg import routes
