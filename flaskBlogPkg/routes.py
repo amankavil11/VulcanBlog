63c19bb9d8ba504a33fc53d12135b6eea9cbfb02
Abe Mathew Mankavil <abe.m.mankavil@gmail.com>
2022-06-30 22:09:20 -0400

    Incorporate Flask-Login session features

--- flaskBlogPkg/routes.py
+++ flaskBlogPkg/routes.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flaskBlogPkg import app, db, bcrypt
 from flaskBlogPkg.forms import *
 from flaskBlogPkg.models import user, post
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -32,9 +33,12 @@ def About():
 
 @app.route("/register", methods=['GET', 'POST'])
 def Register():
+    if current_user.is_authenticated:
+        return redirect(url_for('Home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # noinspection PyArgumentList
         User = user(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(User)
         db.session.commit()
@@ -46,7 +50,33 @@ def Register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def Login():
+    if current_user.is_authenticated:
+        return redirect(url_for('Home'))
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('Home'))
+        User_Email = user.query.filter_by(email=form.username.data).first()
+        User = user.query.filter_by(username=form.username.data).first()
+        if User_Email:
+            User = User_Email
+        if User and bcrypt.check_password_hash(User.password, form.password.data):
+            login_user(User, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash('Login Successful!', 'success')
+            return redirect(next_page) if next_page else redirect(url_for('Home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def LogOut():
+    logout_user()
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('Home'))
+
+
+@app.route("/account")
+@login_required
+def Account():
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file)
