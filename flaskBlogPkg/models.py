2e5a627cf70404008f827bb43f026cbf3b1a4f16
Abe Mathew Mankavil <abe.m.mankavil@gmail.com>
2022-06-30 22:06:57 -0400

    pass UserMixin paramater(creates default login_man methods) to models

--- flaskBlogPkg/models.py
+++ flaskBlogPkg/models.py
@@ -1,27 +1,28 @@
 from datetime import datetime
 from flaskBlogPkg import db, login_man
+from flask_login import UserMixin
 
 
-@login_man.user_loader()
+@login_man.user_loader
 def load_user(user_id):
     return user.query.get(int(user_id))
 
 
 #try to always use lowercase class(aka models) names when using SQLAlchemy
-class user(db.Model):
+class user(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    image_file = db.Column(db.String(20), nullable=False, default="default.png")
     password = db.Column(db.String(60), nullable=False)
-    # 'Post' references model name, hence the capitalization
+    # 'post' references model name, hence the capitalization
     posts = db.relationship('post', backref='author', lazy=True)
     
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class post(db.Model):
+class post(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(280), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
