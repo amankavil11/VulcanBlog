800075ba631ea60262b407522f6e72f7781f9c15
Abe Mathew Mankavil <abe.m.mankavil@gmail.com>
2022-08-10 19:35:04 -0400

    Format ContentType HTTP header for AWS S3

--- flaskBlog/Users/utils.py
+++ flaskBlog/Users/utils.py
@@ -24,13 +24,16 @@ def save_pic(form_pic):
     rand_hex = secrets.token_hex(8)
     # underscore is way of discarding unused variable that wd be produced by splittext function
     _, file_ext = os.path.splitext(form_pic.filename)
+    contentType = file_ext.lower()
+    if contentType == '.jpg':
+        contentType = 'jpeg'
     file_bytes = form_pic.stream.read()
     pic_fn = rand_hex + file_ext
     s3 = boto.client('s3', aws_access_key_id=config('S3_ACCESS_ID'),
                      aws_secret_access_key=config('S3_SECRET_KEY'))
     picture_path = os.path.join('static/profile_pics', pic_fn)
     s3.put_object(ACL='public-read', Body=file_bytes, Bucket=config('S3_BUCKET_NAME'), Key=picture_path,
-                  ContentType='image/' + file_ext)
+                  ContentType='image/' + contentType)
     prev_pic = os.path.join('static/profile_pics', current_user.image_file)
     if current_user.image_file != 'default.png':
         s3.delete_object(Bucket=config('S3_BUCKET_NAME'), Key=prev_pic)
